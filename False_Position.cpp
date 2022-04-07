2ac72da9f59108ccc597283ad383bd3432eaa521
Fahim <dfahim432@gmail.com>
2022-04-07 17:56:12 +0600

    Adding regular falsi method

--- False_Position.cpp
+++ False_Position.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Bisection
+class FalsePosition
 {
 private:
     double eps;
@@ -10,7 +10,7 @@ private:
     double a, b, root;
 
 public:
-    Bisection()
+    FalsePosition()
     {
         a = 0, b = 0, root = -1, eps = 0.0000000001;
     }
@@ -18,7 +18,13 @@ public:
 public:
     double f(double x)
     {
-        return x *x *x - x *x + 2;
+        return x * x * x - x * x + 2;
+    }
+
+public:
+    double getNext(double x0, double x1)
+    {
+        return (x0*f(x1) - x1*f(x0)) / (f(x1) - f(x0));
     }
 
 public:
@@ -45,15 +51,14 @@ public:
     void findRoot()
     {
         findInterval();
-        double mid = (a + b) / 2;
-
+        double mid = getNext(a, b);
+        // Driver program to test above function 
         while (abs(f(b) - f(a)) > eps)
-        {
-            if (f(mid) > 0)
-                b = mid;
-            else
-                a = mid;
-            mid = (a + b) / 2;
+        {            
+            if(f(mid) == 0) break;
+            else if (f(mid)*f(a)<0) b = mid;
+            else a = mid;
+            mid = getNext(a, b);
         }
 
         root = mid;
@@ -62,7 +67,7 @@ public:
 
 int main()
 {
-    Bisection bisection;
-    bisection.findRoot();
-    printf("The root of the given equation : %0.10lf\n", bisection.getRoot());
+    FalsePosition falsePosition;
+    falsePosition.findRoot();
+    printf("The root of the given equation : %0.10lf\n", falsePosition.getRoot());
 }
